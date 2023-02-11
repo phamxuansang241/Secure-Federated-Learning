dacda95f05f4b7b60063c238ce1e2cce3a602835
sangpx <hohieuocc@gmail.com>
2023-02-11 10:25:14 +0700

    fix

--- generate_utils/generate_json.py
+++ generate_utils/generate_json.py
@@ -1,12 +1,47 @@
 import json
 import os
 import shutil
+import copy
+
 
 training_modes = ['fedavg', 'fed_compress', 'fed_ecc', 'fed_elgamal']
 nb_clients = [5, 10, 20, 40, 50]
 datasets = ['csic2010', 'mnist', 'smsspam']
 data_sample_technique = ['iid', 'noniid_labeldir']
-global_epochs = [2, 3]
+global_epochs = [2, 50]
+digits = [3, 5, 10]
+
+config = {
+        "global_config": {
+            "name": "Test",
+            "overwrite_experiment": True,
+            "device": "cuda",
+            "training_mode": 'fedavg',
+            "compress_digit": 3,
+            "dp_mode": False
+        },
+        "data_config": {
+            "dataset_name": 'csic2010',
+            "data_sampling_technique": 'iid'
+        },
+        "fed_config": {
+            "global_epochs": 5,
+            "local_epochs": 1,
+            "nb_clients": 5,
+            "fraction": 1.0,
+            "batch_size": 32
+        },
+        "optim_config": {
+            "lr": 0.001
+        },
+        "dp_config": {
+            "epsilon": 50,
+            "delta": 3e-05,
+            "clipping_norm": 2.0,
+            "is_fixed_client_iter": True,
+            "client_iter": 700
+        }
+    }
 
 for training_mode in training_modes:
     fodler = f'json_files/{training_mode}'
@@ -17,47 +52,29 @@ for training_mode in training_modes:
         for dataset in datasets:
             for technique in data_sample_technique:
                 for ge in global_epochs:
-                    config = {
-                        "global_config": {
-                            "name": "Test",
-                            "overwrite_experiment": True,
-                            "device": "cuda",
-                            "training_mode": training_mode,
-                            "compress_digit": 3,
-                            "dp_mode": False
-                        },
-                        "data_config": {
-                            "dataset_name": dataset,
-                            "data_sampling_technique": technique
-                        },
-                        "fed_config": {
-                            "global_epochs": ge,
-                            "local_epochs": 1,
-                            "nb_clients": nb_client,
-                            "fraction": 1.0,
-                            "batch_size": 32
-                        },
-                        "optim_config": {
-                            "lr": 0.001
-                        },
-                        "dp_config": {
-                            "epsilon": 50,
-                            "delta": 3e-05,
-                            "clipping_norm": 2.0,
-                            "is_fixed_client_iter": True,
-                            "client_iter": 700
-                        }
-                    }
-                    
-                    if training_mode == 'fed_compress':
-                        compress_digit = config['global_config']['compress_digit']
-                        filename = f'config_dg_{compress_digit}.json'
-                    else:
-                        filename = f'config.json'
+                    temp_config = copy.deepcopy(config)
+                    temp_config['global_config']['training_mode'] = training_mode
+                    temp_config['data_config']['dataset_name'] = dataset
+                    temp_config['data_config']['data_sampling_technique'] = technique
+                    temp_config['fed_config']['global_epochs'] = ge
+                    temp_config['fed_config']['nb_clients'] = nb_client
+
+
                     directory = f'json_files/{training_mode}/{dataset}/{nb_client}_clients/{technique}/{ge}_global_epochs'
                     if not os.path.exists(directory):
                         os.makedirs(directory)
-                    filepath = os.path.join(directory, filename)
+
+                    if training_mode == 'fed_compress':
+                        for dg in digits:
+                            temp_config['global_config']['compress_digit'] = dg
+                            filename = f'config_dg_{dg}.json'
+                            filepath = os.path.join(directory, filename)
+
+                            with open(filepath, 'w') as f:
+                                json.dump(temp_config, f, indent=4)
+                    else:
+                        filename = f'config.json'
+                        filepath = os.path.join(directory, filename)
                     
-                    with open(filepath, 'w') as f:
-                        json.dump(config, f, indent=4)
+                        with open(filepath, 'w') as f:
+                            json.dump(temp_config, f, indent=4)
