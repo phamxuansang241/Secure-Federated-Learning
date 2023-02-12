e9fdf612fe2e9c88f221f2a8f80606a3c5b169fa
Px Sang <sang.px195991@sis.hust.edu.vn>
2023-02-12 15:58:18 +0700

    Delete smsspam from main

--- tek4fed/data_lib/__init__.py
+++ tek4fed/data_lib/__init__.py
@@ -3,6 +3,5 @@ from .preprocessing_data_csic2010 import csic2010_load_data
 from .preprocessing_data_fwaf import fwaf_load_data
 from .preprocessing_data_httpparams import httpparams_load_data
 from .preprocessing_data_mnist import mnist_load_data
-from .preprocessing_data_smsspam import smsspam_load_data
 from .text_encode_utils import create_vocab_set, encode_data
 from .data_distribute_heplers import iid_data_indices, non_iid_data_indices, DataHandler
