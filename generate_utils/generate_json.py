7a7cb50d2d1d24337884a652d827d5e76a57271f
sangpx <hohieuocc@gmail.com>
2023-04-17 14:59:34 +0700

    clean generate_json.py

--- generate_utils/generate_json.py
+++ generate_utils/generate_json.py
@@ -1,91 +1,84 @@
+import argparse
 import json
 import os
 import shutil
 import copy
 
 
-training_modes = ['dssgd']
-nb_clients = [10]
-datasets = ['csic2010', 'smsspam']
-# datasets = ['mnist']
-data_sample_technique = ['iid', 'noniid_label_dir']
-# data_sample_technique = ['iid', 'noniid_label_quantity']
-global_epochs = [50]
-digits = [3, 5, 10]
-fraction = [1.0]
-# fraction = [0.9, 0.8, 0.7, 0.6, 0.5]
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Generate configuration files.')
 
-config = {
-        "global_config": {
-            "name": "Test - 50% - Upload",
-            "overwrite_experiment": True,
-            "device": "cuda",
-            "training_mode": 'fedavg',
-            "compress_digit": 15,
-            "dp_mode": False
-        },
-        "data_config": {
-            "dataset_name": 'csic2010',
-            "data_sampling_technique": 'iid'
-        },
-        "fed_config": {
-            "global_epochs": 5,
-            "local_epochs": 1,
-            "nb_clients": 5,
-            "fraction": 1.0,
-            "batch_size": 32
-        },
-        "optim_config": {
-            "lr": 0.001
-        },
-        "dp_config": {
-            "epsilon": 50,
-            "delta": 3e-05,
-            "clipping_norm": 2.0,
-            "is_fixed_client_iter": True,
-            "client_iter": 700
-        }
-    }
+    parser.add_argument('--training_modes', nargs='+', default=['dssgd'], help='List of training modes.')
+    parser.add_argument('--nb_clients', nargs='+', type=int, default=[5, 10, 20, 40, 50], help='List of the number of clients.')
+    parser.add_argument('--datasets', nargs='+', default=['csic2010', 'smsspam'], help='List of dataset names.')
+    parser.add_argument('--data_sample_techniques', nargs='+', default=['iid', 'noniid_label_dir'], help='List of data sampling techniques.')
+    parser.add_argument('--global_epochs', nargs='+', type=int, default=[2, 50], help='List of global epoch values.')
+    parser.add_argument('--fractions', nargs='+', type=float, default=[1.0], help='List of fraction values.')
+    parser.add_argument('--digits', nargs='+', type=int, default=[3, 5, 10], help='List of digits.')
+    parser.add_argument('--sigmas', nargs='+', type=float, default=[0.1], help='List of sigma values.')
+    
+    return parser.parse_args()
 
 
-for training_mode in training_modes:
-    folder = f'generate_utils\generated_files\json_files\{training_mode}'
-    if os.path.isdir(folder):
-        shutil.rmtree(folder)
+def generate_configs(training_modes, nb_clients, datasets, data_sample_techniques, global_epochs, fractions, digits, sigmas, config):
+    for training_mode in training_modes:
+        folder = f'generate_utils/generated_files/json_files/{training_mode}'
+        if os.path.isdir(folder):
+            shutil.rmtree(folder)
 
-    for nb_client in nb_clients:
-        for dataset in datasets:
-            for technique in data_sample_technique:
-                for ge in global_epochs:
-                    for fr in fraction:
+        for nb_client in nb_clients:
+            for dataset in datasets:
+                for technique in data_sample_techniques:
+                    for ge in global_epochs:
+                        for fr in fractions:
+                            for sigma in sigmas:
+                                temp_config = create_config(config, training_mode, dataset, technique, ge, nb_client, fr, sigma)
+                                sub_directory = create_sub_directory(training_mode, dataset, nb_client, technique, ge, fr, sigma)
+                                create_directory(sub_directory)
 
-                        temp_config = copy.deepcopy(config)
-                        temp_config['global_config']['training_mode'] = training_mode
-                        temp_config['data_config']['dataset_name'] = dataset
-                        temp_config['data_config']['data_sampling_technique'] = technique
-                        temp_config['fed_config']['global_epochs'] = ge
-                        temp_config['fed_config']['nb_clients'] = nb_client
-                        temp_config['fed_config']['fraction'] = fr
+                                if training_mode == 'fed_compress':
+                                    for dg in digits:
+                                        write_config(sub_directory, temp_config, dg)
+                                else:
+                                    write_config(sub_directory, temp_config)
 
 
-                        sub_directory = f'generate_utils\generated_files\json_files\{training_mode}\{dataset}\{nb_client}_clients\{technique}\{ge}_global_epochs\{fr}_fraction'
-                    
-                        print(sub_directory)
-                        if not os.path.exists(sub_directory):
-                            os.makedirs(sub_directory)
-                            print(os.path.exists(sub_directory))
 
-                        if training_mode == 'fed_compress':
-                            for dg in digits:
-                                temp_config['global_config']['compress_digit'] = dg
-                                filename = f'config_dg_{dg}.json'
-                                filepath = os.path.join(sub_directory, filename)
+def create_config(config, training_mode, dataset, technique, ge, nb_client, fr, sigma):
+    temp_config = copy.deepcopy(config)
+    temp_config['global_config']['training_mode'] = training_mode
+    temp_config['data_config']['dataset_name'] = dataset
+    temp_config['data_config']['data_sampling_technique'] = technique
+    temp_config['fed_config']['global_epochs'] = ge
+    temp_config['fed_config']['nb_clients'] = nb_client
+    temp_config['fed_config']['fraction'] = fr
+    temp_config['dp_config']['sigma'] = sigma
+    
+    return temp_config
 
-                                with open(filepath, 'w') as f:
-                                    json.dump(temp_config, f, indent=4)
-                        else:
-                            filename = f'config.json'
-                            filepath = os.path.join(sub_directory, filename)
-                        
-                            with open(filepath, 'w') as f:
-                                json.dump(temp_config, f, indent=4)
+
+def create_sub_directory(training_mode, dataset, nb_client, technique, ge, fr, sigma):
+    return f'generate_utils/generated_files/json_files/{training_mode}/{dataset}/{nb_client}_clients/{technique}/{ge}_global_epochs/{fr}_fraction/{sigma}_sigma'
+
+
+def create_directory(sub_directory):
+    if not os.path.exists(sub_directory):
+        os.makedirs(sub_directory)
+
+
+def write_config(sub_directory, config, dg=None):
+    filename = f'config_dg_{dg}.json' if dg else 'config.json'
+    filepath = os.path.join(sub_directory, filename)
+
+    with open(filepath, 'w') as f:
+        json.dump(config, f, indent=4)
+        
+        
+if __name__ == '__main__':
+    args = parse_arguments()
+
+    config_template_file_path = 'generate_utils/config_template.json'
+    with open(config_template_file_path, 'r') as f:
+        config = json.load(f)
+
+    generate_configs(args.training_modes, args.nb_clients, args.datasets, args.data_sample_techniques, args.global_epochs, args.fractions, args.digits, args.sigmas, config)
